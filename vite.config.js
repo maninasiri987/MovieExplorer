d1d5db7dccc193113775fd25712bd175d6b350cf
maninasiri987 <maninasiri987@gmail.com>
2026-06-02 17:03:00 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// for test
+// for test 2
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
